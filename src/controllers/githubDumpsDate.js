679a2a76309435c3d4f87ef8b13099bc9242efb2
Dmitry Kiselman <kiseldimon1@gmail.com>
2024-11-16 14:50:25 +0200

    added cors

--- src/controllers/githubDumpsDate.js
+++ src/controllers/githubDumpsDate.js
@@ -3,11 +3,12 @@ export class DateController {
         res.setHeader('Content-Type', 'text/event-stream');
         res.setHeader('Cache-Control', 'no-cache');
         res.setHeader('Connection', 'keep-alive');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         const gitHubDate = req.params.date;
         clients.push(res);
 
-        if (gitHubDate === 'none' || gitHubDate !== dataInstance?.currentData?.date) {
+        if (gitHubDate === 'null' || gitHubDate !== dataInstance?.currentData?.date) {
             res.write(`UPLOAD_NEW_DATA\n\n`);
         } else {
             res.write('DATA_IS_ACTUAL\n\n');
