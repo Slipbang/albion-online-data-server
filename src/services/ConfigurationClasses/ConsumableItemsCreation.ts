c105cb778cd51550c65650ebab24d53907d97790
Dmitry Kiselman <kiseldimon1@gmail.com>
2024-12-09 00:22:30 +0200

    some variables names fixes

--- src/services/ConfigurationClasses/ConsumableItemsCreation.ts
+++ src/services/ConfigurationClasses/ConsumableItemsCreation.ts
@@ -3,7 +3,7 @@ import {IAppItems, TConsumableCraftItem, TConsumableTypes} from "../dummyItems.j
 
 export class ConsumableItemsCreation {
 
-    private static _buildConsumableResourceObjectHandler (resource: ICraftResourceItem, obj: TConsumableCraftItem) {
+    private static _buildConsumableResourceObjectHandler (resource: ICraftResourceItem, consumableObject: TConsumableCraftItem) {
         const special = ['T1_FISHSAUCE_LEVEL1', 'T1_ALCHEMY_EXTRACT_LEVEL1'];
         let resourceId = resource['@uniquename'];
         const foodConsumptionCount = resourceId.includes('QUESTITEM_TOKEN_AVALON') ? 7.2 : 4.5;
@@ -11,10 +11,10 @@ export class ConsumableItemsCreation {
             resourceId = resourceId.split('_').map(str => (str === 'LEVEL1') ? 'LEVEL' : str).join('_');
         } else {
             if (!resourceId.includes('_FISH_') && !resourceId.includes('_ALCHEMY_')) {
-                obj.foodConsumption += +resource["@count"] * foodConsumptionCount;
+                consumableObject.foodConsumption += +resource["@count"] * foodConsumptionCount;
             }
         }
-        obj[resourceId] = +resource["@count"];
+        consumableObject[resourceId] = +resource["@count"];
     }
 
     private static _CONSUMABLE_CATEGORIES = ['potion', 'cooked'];
@@ -27,7 +27,7 @@ export class ConsumableItemsCreation {
                 const craftResources = item["enchantments"]!["enchantment"][0]['craftingrequirements']['craftresource'];
                 const itemId = item["@uniquename"];
                 if (craftResources) {
-                    let obj: TConsumableCraftItem = {
+                    let consumableObject: TConsumableCraftItem = {
                         itemId,
                         foodConsumption: 0,
                         amountCrafted: +(item['craftingrequirements'] as ICraftingRequirements)?.['@amountcrafted']!,
@@ -35,13 +35,13 @@ export class ConsumableItemsCreation {
 
                     if (Array.isArray(craftResources)) {
                         for (let resource of craftResources) {
-                            this._buildConsumableResourceObjectHandler(resource, obj)
+                            this._buildConsumableResourceObjectHandler(resource, consumableObject)
                         }
                     } else {
-                        this._buildConsumableResourceObjectHandler(craftResources, obj)
+                        this._buildConsumableResourceObjectHandler(craftResources, consumableObject)
                     }
 
-                    items.consumableCraftItems[shopsubcategory1].push(obj);
+                    items.consumableCraftItems[shopsubcategory1].push(consumableObject);
                 }
             }
         }
